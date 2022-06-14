12f769daae769f88d15b8a000030a1ddb886439f
cscs <phone18041361203@163.com>
2022-06-15 00:08:39 +0800

    .

--- src/main/java/Demo4.java
+++ src/main/java/Demo4.java
@@ -8,6 +8,8 @@ public class Demo4 {
     public static void main(String[] args) {
         String pwd = Demo4.class.getResource(".").getPath();
         renderImage(pwd+"./123.html",pwd+"./123.png");
+        String osName = System.getProperties().getProperty("os.name");
+        System.out.println(osName);
 
     }
 
@@ -15,11 +17,24 @@ public class Demo4 {
     //请求网页渲染生成图片
     public static void renderImage(String httpUrl,String outputImagePath){
         try {
+
+            String command=null;
+
+            //判断当前操作系统类型
+            if (OSInfo.isMacOS()|| OSInfo.isMacOSX()) {
+                command="phantomjs-mac";
+            }else if (OSInfo.isLinux()){
+                command="phantomjs-linux";
+            }else {
+                throw new RuntimeException("只支持windows和linux系统");
+            }
+
+
             String pwd = Demo4.class.getResource(".").getPath();
-            String command = String.format("%sphantomjs %srasterize.js %s %s", pwd, pwd, httpUrl, outputImagePath);
+            String commandLine = String.format("%s%s %srasterize.js %s %s", pwd,command, pwd, httpUrl, outputImagePath);
             System.out.println("command:[]"+command);
 
-            String[] cmd = new String[]{"/bin/sh", "-c", command};
+            String[] cmd = new String[]{"/bin/sh", "-c", commandLine};
             Process ps = Runtime.getRuntime().exec(cmd);
 
 
