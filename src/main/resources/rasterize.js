3f168b77b70c9744619b7bf883d8671329cf0595
cscs <phone18041361203@163.com>
2022-06-21 05:45:10 +0800

    .

--- src/main/resources/rasterize.js
+++ src/main/resources/rasterize.js
@@ -1,9 +1,10 @@
 "use strict";
 var page = require('webpage').create(),
     system = require('system'),
-    address, output, size;
+    address, output, size,
+    data
 
-if (system.args.length < 3 || system.args.length > 5) {
+if (system.args.length < 4 || system.args.length > 6) {
     console.log('Usage: rasterize.js URL filename [paperwidth*paperheight|paperformat] [zoom]');
     console.log('  paper (pdf output) examples: "5in*7.5in", "10cm*20cm", "A4", "Letter"');
     console.log('  image (png/jpg output) examples: "1920px" entire page, window width 1920px');
@@ -13,28 +14,30 @@ if (system.args.length < 3 || system.args.length > 5) {
     address = system.args[1];
     output = system.args[2];
     page.viewportSize = { width: 600, height: 600 };
-    if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
-        size = system.args[3].split('*');
-        page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
-                                           : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
-    } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
-        size = system.args[3].split('*');
-        if (size.length === 2) {
-            pageWidth = parseInt(size[0], 10);
-            pageHeight = parseInt(size[1], 10);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-            page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
-        } else {
-            console.log("size:", system.args[3]);
-            pageWidth = parseInt(system.args[3], 10);
-            pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
-            console.log ("pageHeight:",pageHeight);
-            page.viewportSize = { width: pageWidth, height: pageHeight };
-        }
-    }
-    if (system.args.length > 4) {
-        page.zoomFactor = system.args[4];
-    }
+    // if (system.args.length > 3 && system.args[2].substr(-4) === ".pdf") {
+    //     size = system.args[3].split('*');
+    //     page.paperSize = size.length === 2 ? { width: size[0], height: size[1], margin: '0px' }
+    //                                        : { format: system.args[3], orientation: 'portrait', margin: '1cm' };
+    // } else if (system.args.length > 3 && system.args[3].substr(-2) === "px") {
+    //     size = system.args[3].split('*');
+    //     if (size.length === 2) {
+    //         pageWidth = parseInt(size[0], 10);
+    //         pageHeight = parseInt(size[1], 10);
+    //         page.viewportSize = { width: pageWidth, height: pageHeight };
+    //         page.clipRect = { top: 0, left: 0, width: pageWidth, height: pageHeight };
+    //     } else {
+    //         console.log("size:", system.args[3]);
+    //         pageWidth = parseInt(system.args[3], 10);
+    //         pageHeight = parseInt(pageWidth * 3/4, 10); // it's as good an assumption as any
+    //         console.log ("pageHeight:",pageHeight);
+    //         page.viewportSize = { width: pageWidth, height: pageHeight };
+    //     }
+    // }
+    // if (system.args.length > 4) {
+    //     page.zoomFactor = system.args[4];
+    // }
+    data= JSON.parse(system.args[3])
+    // ./phantomjs-mac rasterize.js 1.html 3.jpg '[{"categories":["123","456"],"series":[{"name":"123","data":"1"},{"name":"456","data":"1"}]}]'
     page.open(address, function (status) {
         if (status !== 'success') {
             console.log('Unable to load the address!');
@@ -45,30 +48,12 @@ if (system.args.length < 3 || system.args.length > 5) {
             console.log(echarts)
             var loadecharts=page.injectJs("/Users/zhangquan/Desktop/temp/excel_Ewechat/src/main/resources/1.js")
             console.log(loadecharts)
-
-            page.evaluate(function () {
+            console.log("收到参数"+data)
+            page.evaluate(function (data) {
                 document.body.bgColor = 'white';
                 console.log("执行函数")
-                data=[{
-                    categories:["123","456"],
-                    series :[
-                        {name:"123",
-                            data: "1"
-                        },
-                        {name:"456",
-                            data: "1"
-                        }
-                    ]
-                }
-
-                ]
                 echarts_1(data)
-            })
-
-
-
-
-
+            },data)
             window.setTimeout(function () {
                 console.log("开始render")
                 page.render(output);
